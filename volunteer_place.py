f899d00b949fcdf0404c81d50bc94237f5f70678
Noam <noamb0304@gmail.com>
2023-09-20 13:18:38 +0300

    dict commit

--- volunteer_place.py
+++ volunteer_place.py
@@ -2,3 +2,10 @@ import consts
 import random
 
 def random_volunteer(list):
+    random_vol = random.choice(list)
+    return random_vol
+
+
+def remove_volunteer(list, current_vol):
+    list.remove(current_vol)
+    return list
