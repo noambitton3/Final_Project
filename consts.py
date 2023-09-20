25c43aa6568ed8d74ec35a5775bd9816e2de56a5
try <noamb0304@gmail.com>
2023-09-20 12:10:32 +0300

    consts start commit

--- consts.py
+++ consts.py
@@ -1,7 +1,15 @@
 CITY_LIST_COMMUNITY_SERVICE = ['Ramat Gan', 'Tel Aviv', 'Bat Yam', 'Ashdod', 'Haifa', 'Petah Tikva']
-COMMUNITY_SERVICE = {'Ramat Gan': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
-                     'Tel Aviv': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
+COMMUNITY_SERVICE = {'Ramat Gan': {'elderly people': ["עמך- ביקורי בית לניצולי שואה",
+                                                      "יחידות סגולה- סיוע לקשישים מאושפזים במיצוי זכויותיהם",
+                                                      "מטב- הפגת בדידות לקשישים בביקורים",
+                                                      "ביקור קשישים מאושפזים בבית החולים",
+                                                      "מטב- ליווי ותמיכה בקשישים עם בעלי חיים"],
+                                   'Children': [], 'Animals': [], 'Earth': []},
+                     'Tel Aviv': {'elderly people': ["מטב- התנדבות טלפונית",
+                                                     "מטב- הפעלות וסדנאות לקשישים"], 'Children': [], 'Animals': [], 'Earth': []},
 'Bat Yam': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
 'Ashdod': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
 'Haifa': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
 'Petah Tikva': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []}}
+
+print(COMMUNITY_SERVICE['Ramat Gan']['elderly people'][4])
