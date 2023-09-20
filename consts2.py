bd0a78e2dea5d59b2bdd08ce77b9135da1a65d2f
Noam <noamb0304@gmail.com>
2023-09-20 16:59:31 +0300

    smile commit

--- consts2.py
+++ consts2.py
@@ -2,7 +2,7 @@ import pygame
 
 TITLE_CALL_VISIT = "Call or Visit"
 TEXT_CALL_VISIT = "Do something simple: Call your grandparents, friends, visit your family..."
-TEXT_CALL_VISIT_DOWN = "Even the smalest action can change someone's day!"
+TEXT_CALL_VISIT_DOWN = "Even the smallest action can change someone's day!"
 TEXT_CV_SIZE = 27
 TEXT_CV_LOCATION = (140, 200)
 TEXT_CV_LOCATION_DOWN = (150, 650)
@@ -11,3 +11,17 @@ PLAY_PHONE_PHOTO = pygame.image.load('phone.png')
 PLAY_PHONE_PHOTO = pygame.transform.scale(PLAY_PHONE_PHOTO, (300, 200))
 PLAY_GRANDMA_PHOTO = pygame.image.load('grandma.png')
 PLAY_GRANDMA_PHOTO = pygame.transform.scale(PLAY_GRANDMA_PHOTO, (300, 400))
+
+TITLE_SMILE = 'Smile to the world'
+TEXT_SMILE1 = 'If you go outside today,'
+TEXT_SMILE2 = 'Smile !'
+TEXT_SMILE3 = "It can make someone's day!"
+TITLE_SMILE_SIZE = 100
+TITLE_SMILE_LOCATION = (100, 40)
+TEXT_SMILE1_LOCATION = (250,200)
+TEXT_SMILE2_LOCATION = (360,300)
+TEXT_SMILE3_LOCATION = (200,400)
+TEXT_1_3_SIZE = 50
+TEXT_2_SIZE = 80
+PLAY_SMILE_PHOTO = pygame.image.load('smile_2.png')
+PLAY_SMILE_PHOTO = pygame.transform.scale(PLAY_SMILE_PHOTO, (250, 250))
---
+++ smile.py
@@ -0,0 +1,24 @@
+import pygame
+import consts
+import consts2
+import screen
+
+
+def create_smile_screen():
+    screen.screen.fill(consts.BACKGROUND_COLOR)
+    draw_title_smile()
+    screen.screen.blit(consts2.PLAY_SMILE_PHOTO, (350, 450))
+    smile_explanation(consts2.TEXT_SMILE1, consts2.TEXT_SMILE1_LOCATION, consts2.TEXT_1_3_SIZE)
+    smile_explanation(consts2.TEXT_SMILE2, consts2.TEXT_SMILE2_LOCATION, consts2.TEXT_2_SIZE)
+    smile_explanation(consts2.TEXT_SMILE3, consts2.TEXT_SMILE3_LOCATION, consts2.TEXT_1_3_SIZE)
+    pygame.display.flip()
+
+
+def draw_title_smile():
+    screen.draw_title(consts2.TITLE_SMILE, consts2.TITLE_SMILE_SIZE,
+                      consts.STARTING_COLOR, consts2.TITLE_SMILE_LOCATION)
+
+
+def smile_explanation(text, location, size):
+    screen.draw_text(text, size,
+                     consts.EXPLAIN_COLOR, location)
