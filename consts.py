0176648c76905b18cb1502453c002e6618d5bb2d
try <noamb0304@gmail.com>
2023-09-20 11:49:05 +0300

    consts commit

---
+++ consts.py
@@ -0,0 +1,7 @@
+CITY_LIST_COMMUNITY_SERVICE = ['Ramat Gan', 'Tel Aviv', 'Bat Yam', 'Ashdod', 'Haifa', 'Petah Tikva']
+COMMUNITY_SERVICE = {'Ramat Gan': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
+                     'Tel Aviv': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
+'Bat Yam': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
+'Ashdod': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
+'Haifa': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []},
+'Petah Tikva': {'elderly people': [], 'Children': [], 'Animals': [], 'Earth': []}}
