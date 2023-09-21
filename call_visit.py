620981399ca42dbe2975d79e5da7e90d994e4c3a
lianm <lianmitbarg@gmail.com>
2023-09-21 10:48:51 +0300

    call visit

--- call_visit.py
+++ call_visit.py
@@ -7,11 +7,12 @@ import screen
 def create_screen():
     screen.screen.fill(consts.BACKGROUND_COLOR)
     draw_title_call_visit()
-    screen.screen.blit(consts2.PLAY_PHONE_PHOTO, (150, 350))
-    screen.screen.blit(consts2.PLAY_GRANDMA_PHOTO, (550, 250))
+    screen.screen.blit(consts2.PLAY_PHONE_PHOTO, (100, 230))
+    screen.screen.blit(consts2.PLAY_GRANDMA_PHOTO, (450, 175))
     call_visit_explanation()
     call_visit_down_text()
     pygame.display.flip()
+
 
 def draw_title_call_visit():
     screen.draw_title(consts2.TITLE_CALL_VISIT, consts.STARTING_FONT_SIZE,
--- consts2.py
+++ consts2.py
@@ -3,14 +3,14 @@ import pygame
 TITLE_CALL_VISIT = "Call or Visit"
 TEXT_CALL_VISIT = "Do something simple: Call your grandparents, friends, visit your family..."
 TEXT_CALL_VISIT_DOWN = "Even the smallest action can change someone's day!"
-TEXT_CV_SIZE = 27
-TEXT_CV_LOCATION = (140, 200)
-TEXT_CV_LOCATION_DOWN = (150, 650)
-TEXT_CV_SIZE_DOWN = 35
+TEXT_CV_SIZE = 22
+TEXT_CV_LOCATION = (75, 150)
+TEXT_CV_LOCATION_DOWN = (95, 440)
+TEXT_CV_SIZE_DOWN = 25
 PLAY_PHONE_PHOTO = pygame.image.load('phone.png')
-PLAY_PHONE_PHOTO = pygame.transform.scale(PLAY_PHONE_PHOTO, (300, 200))
+PLAY_PHONE_PHOTO = pygame.transform.scale(PLAY_PHONE_PHOTO, (200, 150))
 PLAY_GRANDMA_PHOTO = pygame.image.load('grandma.png')
-PLAY_GRANDMA_PHOTO = pygame.transform.scale(PLAY_GRANDMA_PHOTO, (300, 400))
+PLAY_GRANDMA_PHOTO = pygame.transform.scale(PLAY_GRANDMA_PHOTO, (200, 250))
 
 TITLE_SMILE = 'Smile to the world'
 TEXT_SMILE1 = 'If you go outside today,'
