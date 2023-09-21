8dd4a0310503aeba8a5a0768fa021529346b0154
lianm <lianmitbarg@gmail.com>
2023-09-21 17:10:26 +0300

    final

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ state = {
     "food_window_open": False,
     "money_window_open": False,
     "second_money_window_open": False,
-    'recycling_window_open': False
+    "recycling_window_open": False,
+    "smile_visit_window_open": False
 }
 
 
@@ -35,6 +36,8 @@ def main():
             money_handle_user_events()
         if state['recycling_window_open']:
             recycling_handle_user_events()
+        if state["smile_visit_window_open"]:
+            smile_visit_handle_user_events()
     pygame.display.flip()
 
 
@@ -61,6 +64,11 @@ def second_handle_user_events():
             if 440 <= mouse[0] <= 690 and 350 <= mouse[1] <= 500:
                 screen2.deed = screen2.choose_deed()
                 screen2.draw_screen(screen2.deed)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                state["second_window_open"] = False
+                state["first_window_open"] = True
+                screen.draw_screen()
 
 
 def food_handle_user_events():
@@ -75,6 +83,11 @@ def food_handle_user_events():
                 food_donate.jerusalem()
             if 500 <= mouse[0] <= 700 and 200 <= mouse[1] <= 300:
                 food_donate.petah_tikva()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                state["food_window_open"] = False
+                state["first_window_open"] = True
+                screen.draw_screen()
 
 
 def money_handle_user_events():
@@ -121,6 +134,22 @@ def recycling_handle_user_events():
             #     food_donate.jerusalem()
             # if 500 <= mouse[0] <= 700 and 200 <= mouse[1] <= 300:
             #     food_donate.petah_tikva()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                state["recycling_window_open"] = False
+                state["first_window_open"] = True
+                screen.draw_screen()
+
+
+def smile_visit_handle_user_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                state["smile_visit_window_open"] = False
+                state["first_window_open"] = True
+                screen.draw_screen()
 
 
 def mouse_location():
@@ -130,8 +159,12 @@ def mouse_location():
 def decide(name):
     if name == "call or visit":
         call_visit.create_screen()
+        state["smile_visit_window_open"] = True
+        state["second_window_open"] = False
     elif name == "smile to the world":
         smile.create_smile_screen()
+        state["smile_visit_window_open"] = True
+        state["second_window_open"] = False
     elif name == "food donation":
         food_donate.create_screen()
         state["food_window_open"] = True
